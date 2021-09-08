0d840a66f8af81d3807c8e169a19ca9aae746b96
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-08 16:54:29 -0400

    3) Highlight Space Debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,11 +12,12 @@ export class Satellite {
 		this.launchDate = launchDate;
 		this.orbitType = orbitType;
 		this.operational = operational;
-   }
-	
+	}
+
 	isSpaceDebris(): boolean {
-		return true;
-   }
+		return this.type === 'Space Debris';
+
+	}
 
 }
 
